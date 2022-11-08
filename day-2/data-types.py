d6b1bf67c5dfbbd6ba496ebbcd6a2addbfc7fc2a
Sukruta Prasanna Pardeshi <67223554+sukruta230901@users.noreply.github.com>
2022-11-08 18:10:47 +0530

    Create data-types.py

---
+++ day-2/data-types.py
@@ -0,0 +1,9 @@
+# Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+two_digit_number = input("Type a two digit number: ")
+
+#Write your code below this line 👇
+num1 = two_digit_number[0]
+num2 = two_digit_number[1]
+add = int(num1) + int(num2)
+print(add)
